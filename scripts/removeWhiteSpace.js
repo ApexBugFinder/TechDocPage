9d308c5c737de9adcb8caa86d926c4f616a5f1e8
Kong Kunru <apexbugfinder@gmail.com>
2021-03-27 22:29:20 -0700

    solved problem with  code block

--- scripts/removeWhiteSpace.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('codeInputBlock').addEventListener('load', removeWhiteSpaceFunction);
-
-const codeBlock
-function removeWhiteSpaceFunction() {
-    console.log('hello from remove white space function');
-    var codeInputBloc = document.getElementById('codeInputBlock');
-
-    // codeInputBloc.nextSibling.parentNode.removeChild(element.nextSibling);
-}
